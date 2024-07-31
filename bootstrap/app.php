3691997dfd23664248c4172ac4dd078f8a8edea1
Shift <shift@laravelshift.com>
2024-07-31 12:50:03 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,34 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->validateCsrfTokens(except: [
+            '/api/v1/*',
+        ]);
+
+        $middleware->append(\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class);
+
+        $middleware->web([
+            \App\Http\Middleware\FrameGuard::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+        ]);
+
+
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+            'api.admin' => \App\Http\Middleware\Api\Admin::class,
+            'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'dangerzone' => \App\Http\Middleware\DangerZone::class,
+            'interstitial' => \App\Http\Middleware\AccountInterstitial::class,
+            'localization' => \App\Http\Middleware\Localization::class,
+            'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
+            'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
+            'twofactor' => \App\Http\Middleware\TwoFactorAuth::class,
+            'validemail' => \App\Http\Middleware\EmailVerificationCheck::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
