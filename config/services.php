31e819b7be3908b21b25737d5289acaba5d7dc47
Shift <shift@laravelshift.com>
2024-07-31 12:50:01 +0000

    Streamline config files

--- config/services.php
+++ config/services.php
@@ -2,27 +2,9 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
-    | default location for this type of information, allowing packages
-    | to have a conventional place to find your various credentials.
-    |
-    */
-
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
-    ],
-
-    'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => env('SES_REGION', 'us-east-1'),
     ],
 
     'sparkpost' => [
@@ -38,4 +20,5 @@ return [
     'expo' => [
         'access_token' => env('EXPO_ACCESS_TOKEN'),
     ],
+
 ];
